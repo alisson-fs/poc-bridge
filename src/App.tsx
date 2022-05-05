93d335360fcff6aa50ba8154bb7e82adbde780c4
Alisson Fabra da Silva <alisson.fs@bridge.ufsc.br>
2022-05-05 18:49:11 -0300

    Aplicação do useState para os calculos e do Context para adicionar uma frase motivacional

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import React from 'react';
 import './App.css';
 import { Formulario } from './components/Formulario';
 
+const FraseMotivacionalContext = React.createContext("");
+
 function App() {
+
   return (
-    <>
+    <FraseMotivacionalContext.Provider value="É hora de esquecer os erros do passado e começar a planejar os erros do futuro."> 
+      <FraseMotivacional />
       <h2>Calculadora de IMC</h2><br />
       <Formulario/>
-    </>
+    </FraseMotivacionalContext.Provider>
   );
+}
+
+function FraseMotivacional() {
+  return (
+    <FraseMotivacionalContext.Consumer>
+      {value => <p>{value}</p>}
+    </FraseMotivacionalContext.Consumer>
+  )
 }
 
 export default App;
--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,13 +1,60 @@
+import React, { useState } from "react";
 import { CampoTexto } from "./CampoTexto";
 
+function Imc(altura: number, peso: number) {
+  return (
+    peso / (altura ** 2)
+  );
+}
+
+interface IMCFormModel {
+  altura: string
+  peso: string
+  resultado: string
+}
+
 export function Formulario() {
+
+  function calcularImc(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    var n_altura = 0;
+    var n_peso = 0;
+    if (imcFormValues.altura !== "") {
+      n_altura = Number(imcFormValues.altura) / 100;
+    }
+    if (imcFormValues.peso !== "") {
+      n_peso = Number(imcFormValues.peso);
+    } 
+    var resultado = "";
+    if (n_altura !== 0 && n_peso !== 0) {
+      resultado = String(Imc(n_altura, n_peso))
+    }
+    handleChangeResultado(resultado)
+  }
+
+  const [imcFormValues, setImcFormValues] = useState<IMCFormModel>({altura: "", peso: "", resultado: ""})
+
+  const handleChangeAltura = (altura: string) => {
+    setImcFormValues({altura, peso: imcFormValues.peso, resultado: imcFormValues.resultado})
+  }
+
+  const handleChangePeso = (peso: string) => {
+    setImcFormValues({altura: imcFormValues.altura, peso, resultado: imcFormValues.resultado})
+  }
+
+  const handleChangeResultado = (resultado: string) => {
+    setImcFormValues({altura: imcFormValues.altura, peso: imcFormValues.peso, resultado})
+  }
+
   const altura = "altura";
   const peso = "peso";
   return (
-    <form>
-      <CampoTexto for={altura} id={altura} name={altura} text="Altura (cm):" />
-      <CampoTexto for={peso} id={peso} name={peso} text="Peso (kg):" />
-      <input type="button" value="Calcular" />
+    <form onSubmit={calcularImc}>
+      <CampoTexto for={altura} id={altura} name={altura} text="Altura (cm):" onChange={handleChangeAltura} />
+      <CampoTexto for={peso} id={peso} name={peso} text="Peso (kg):" onChange={handleChangePeso} />
+      <button type="submit">Calcular</button><br />
+      <label>IMC:</label>
+      <p>{imcFormValues.resultado}</p>
     </form>
   );
 }
