1586122bda4af1ca7810bf48a10236d4d3408cdf
Alisson Fabra da Silva <alisson.fs@bridge.ufsc.br>
2022-07-29 11:21:58 -0300

    Testes para os metodos validateRequired, validateOnlyNumbers e validateGreaterThan

--- src/components/Fomulario/validate.ts
+++ src/components/Fomulario/validate.ts
@@ -1,32 +1,32 @@
 import { ALTURA, PESO } from "./model";
 import { ErrorObject } from "../../validation/model";
-import { validateRequired, validateOnlyNumbers, validateGreaterThan } from "../../validation/validate";
+import {
+  validateRequired,
+  validateOnlyNumbers,
+  validateGreaterThan,
+} from "../../validation/validate";
 import { IMCFormModel } from "./Formulario";
 
+export function validateAltura(
+  errors: ErrorObject<IMCFormModel>,
+  values: IMCFormModel
+) {
+  errors = validateRequired(errors, values, ALTURA);
+  errors = validateOnlyNumbers(errors, values, ALTURA);
+  errors = validateGreaterThan(errors, values, ALTURA, 0);
+  return errors;
+}
 
-function validateAltura(
-    errors: ErrorObject<IMCFormModel>,
-    values: IMCFormModel
-  ) {
-    errors = validateRequired(errors, values, ALTURA);
-    errors = validateOnlyNumbers(errors, values, ALTURA);
-    errors = validateGreaterThan(errors, values, ALTURA, 0);
-    return errors;
-  }
-  
-  function validatePeso(errors: ErrorObject<IMCFormModel>, values: IMCFormModel) {
-    errors = validateRequired(errors, values, PESO);
-    errors = validateOnlyNumbers(errors, values, PESO);
-    errors = validateGreaterThan(errors, values, PESO, 0);
-    return errors;
-  }
-  
-  export function validateIMC(
-    values: IMCFormModel
-  ) {
-    var errors: ErrorObject<IMCFormModel> = {};
-    errors = validateAltura(errors, values);
-    errors = validatePeso(errors, values);
-    return errors;
-  }
-  
+export function validatePeso(errors: ErrorObject<IMCFormModel>, values: IMCFormModel) {
+  errors = validateRequired(errors, values, PESO);
+  errors = validateOnlyNumbers(errors, values, PESO);
+  errors = validateGreaterThan(errors, values, PESO, 0);
+  return errors;
+}
+
+export function validateIMC(values: IMCFormModel) {
+  var errors: ErrorObject<IMCFormModel> = {};
+  errors = validateAltura(errors, values);
+  errors = validatePeso(errors, values);
+  return errors;
+}
--- src/validation/__test__/validate.test.ts
+++ src/validation/__test__/validate.test.ts
@@ -1,29 +1,64 @@
 import { IMCFormModel } from "../../components/Fomulario/Formulario";
-import { ErrorObject, REQUIRED } from "../model";
-import { validateRequired } from "../validate";
+import { ErrorObject, GREATER_THAN, ONLY_NUMBERS, REQUIRED } from "../model";
+import { validateGreaterThan, validateOnlyNumbers, validateRequired } from "../validate";
 
 const ALTURA = "altura";
 
 describe("validate", () => {
-  
   describe("validateRequired", () => {
-    it("null", () => {
-        var errors: ErrorObject<IMCFormModel> = {};
-        const values: IMCFormModel  = { altura: null };
-        errors = validateRequired(errors, values, ALTURA);
-        expect(errors.altura).toBe(REQUIRED);
+    it("quando o campo for null deve retornar erro", () => {
+      var errors: ErrorObject<IMCFormModel> = {};
+      const values: IMCFormModel = { altura: null };
+      errors = validateRequired(errors, values, ALTURA);
+      expect(errors.altura).toBe(REQUIRED);
     });
-    it("undefined", () => {
-        var errors: ErrorObject<IMCFormModel> = {};
-        const values: IMCFormModel = {};
-        errors = validateRequired(errors, values, ALTURA);
-        expect(errors.altura).toBe(REQUIRED);
+    it("quando o campo for undefined deve retornar erro", () => {
+      var errors: ErrorObject<IMCFormModel> = {};
+      const values: IMCFormModel = {};
+      errors = validateRequired(errors, values, ALTURA);
+      expect(errors.altura).toBe(REQUIRED);
     });
-    it("feliz", () => {
-        var errors: ErrorObject<IMCFormModel> = {};
-        const values: IMCFormModel = {altura: 173};
-        errors = validateRequired(errors, values, ALTURA);
-        expect(errors.altura).toBeUndefined();
+    it("quando o campo seja valido nao deve retornar erro", () => {
+      var errors: ErrorObject<IMCFormModel> = {};
+      const values: IMCFormModel = { altura: "173" };
+      errors = validateRequired(errors, values, ALTURA);
+      expect(errors.altura).toBeUndefined();
+    });
+  });
+
+  describe("validateOnlyNumbers", () => {
+    it("quando o campo nao for um numero deve retornar erro", () => {
+      var errors: ErrorObject<IMCFormModel> = {};
+      const values: IMCFormModel = { altura: "abc" };
+      errors = validateOnlyNumbers(errors, values, ALTURA);
+      expect(errors.altura).toBe(ONLY_NUMBERS);
+    });
+    it("quando o campo for um numero nao deve retornar erro", () => {});
+      var errors: ErrorObject<IMCFormModel> = {};
+      const values: IMCFormModel = { altura: "173" };
+      errors = validateOnlyNumbers(errors, values, ALTURA);
+      expect(errors.altura).toBeUndefined();
+  });
+
+  describe("validateGreaterThan", () => {
+    it("quando o campo for menor do que o valor definido deve retornar erro", () => {
+      var errors: ErrorObject<IMCFormModel> = {};
+      const values: IMCFormModel = { altura: "-1" };
+      errors = validateGreaterThan(errors, values, ALTURA, 0);
+      expect(errors.altura).toBe(GREATER_THAN + "0");
+    });
+    it("quando o campo for igual ao valor definidonao deve retornar erro", () => {
+      var errors: ErrorObject<IMCFormModel> = {};
+      const limit = "0" 
+      const values: IMCFormModel = { altura: limit };
+      errors = validateGreaterThan(errors, values, ALTURA, 0);
+      expect(errors.altura).toBe(GREATER_THAN + limit);
+    });
+    it("quando o campo for maior do que o valor definidonao nao deve retornar erro", () => {
+      var errors: ErrorObject<IMCFormModel> = {};
+      const values: IMCFormModel = { altura: "1" };
+      errors = validateGreaterThan(errors, values, ALTURA, 0);
+      expect(errors.altura).toBeUndefined();
     });
   });
 });
