8ac6b5a172d8d3915447deda3bc8e9b0cb9ab2ac
Alisson Fabra da Silva <alisson.fs@bridge.ufsc.br>
2022-09-28 17:34:14 -0300

    Adicionando validação

--- backend/src/main/kotlin/br/alisson/poc/ImcUtils.kt
+++ backend/src/main/kotlin/br/alisson/poc/ImcUtils.kt
@@ -5,12 +5,7 @@ import kotlin.math.pow
 class ImcUtils {
     companion object {
         fun calcular(altura: Double, peso: Double): Double {
-            if (altura != 0.0) {
-                return peso / (altura / 100.0).pow(2.0)
-            } else {
-                throw IllegalArgumentException("A altura deve ser diferente de zero.")
-            }
-
+            return peso / (altura / 100.0).pow(2.0)
         }
     }
 }
