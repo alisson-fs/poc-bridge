3206a6d3d4511aaf90854d15d11c37b762dc9eec
Alisson Fabra da Silva <alisson.fs@bridge.ufsc.br>
2022-09-29 10:48:47 -0300

    Adicionando CrossOrigin

--- backend/src/main/kotlin/br/alisson/poc/controller/CalcularImcController.kt
+++ backend/src/main/kotlin/br/alisson/poc/controller/CalcularImcController.kt
@@ -4,6 +4,7 @@ import br.alisson.poc.ImcUtils
 import br.alisson.poc.model.ImcDto
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +12,7 @@ import kotlin.math.pow
 
 @RestController
 class CalcularImcController {
+    @CrossOrigin(origins = ["http://localhost:3000"])
     @PostMapping("/calcularImc")
     fun calcularImc(@RequestBody values: ImcDto): ResponseEntity<String> {
         if (values.altura > 0.0 && values.peso > 0.0) {
