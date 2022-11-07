db46937630a514f14a6bc303611cbeac7d6ea55a
AlissonFS <alisson.fs@bridge.ufsc.br>
2022-11-07 13:51:12 -0300

    Criando teste de integração para o service e retirando o do controller

---
+++ backend/src/test/kotlin/br/alisson/poc/service/ImcServiceIntegrationTest.kt
@@ -0,0 +1,25 @@
+package br.alisson.poc.service
+
+import br.alisson.poc.model.Imc
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import javax.persistence.EntityManager
+
+@SpringBootTest
+class ImcServiceIntegrationTest(@Autowired private val service: ImcService, @Autowired private val entityManager: EntityManager) {
+
+    @Test
+    fun `calcularPersistirImc deve retornar o imc correto`() {
+        val altura = 100.0
+        val peso = 50.0
+        val expected = "50.0"
+
+        val result = service.calcularPersistirImc(altura, peso)
+
+        assertEquals(expected, result)
+        assertThat(entityManager.find(Imc::class.java, 1L)).isNotNull
+    }
+}
