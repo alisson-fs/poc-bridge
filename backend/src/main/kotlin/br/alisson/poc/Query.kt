739d78e88de04bd9bf86e2c600207183a5e82e31
Alisson Fabra da Silva <alisson@MacBook-Air-de-Alisson.local>
2022-11-17 20:50:33 -0300

    Hello World

--- backend/src/main/kotlin/br/alisson/poc/Query.kt
+++ backend/src/main/kotlin/br/alisson/poc/Query.kt
@@ -1,7 +1,9 @@
 package br.alisson.poc
 
 import graphql.kickstart.tools.GraphQLQueryResolver
+import org.springframework.stereotype.Component
 
+@Component
 class Query : GraphQLQueryResolver {
     fun helloWorld(): String {
         return "Hello World"
