7742a81575fac843a18a8ced5a3562551a0b1094
AlissonFS <alisson.fs@bridge.ufsc.br>
2022-12-08 11:18:14 -0300

    Troca da geração do histórico para lazy query

--- frontend/src/components/Historico.tsx
+++ frontend/src/components/Historico.tsx
@@ -1,11 +1,14 @@
-import { DataTable } from "bold-ui";
-import { Imc, useImcQuery } from "../utils/__generated__/graphql";
+import { Button, DataTable, VFlow } from "bold-ui";
+import {
+  Imc,
+  useImcLazyQuery
+} from "../utils/__generated__/graphql";
 import { ALTURA, PESO } from "./Fomulario/model";
 
 export function Historico() {
-  const { data, loading } = useImcQuery();
+  const [executeImcQuery, { data, loading }] = useImcLazyQuery();
 
-  console.log(data, loading)
+  console.log(data, loading);
 
   const renderAltura = (imc: Imc) => imc.altura;
   const renderPeso = (imc: Imc) => imc.peso;
@@ -14,32 +17,37 @@ export function Historico() {
 
   return (
     <>
-      <DataTable<Imc>
-        columns={[
-          {
-            header: "Altura (cm)",
-            name: ALTURA,
-            render: renderAltura,
-          },
-          {
-            header: "Peso (kg)",
-            name: PESO,
-            render: renderPeso,
-          },
-          {
-            header: "IMC",
-            name: "imc",
-            render: renderImc,
-          },
-          {
-            header: "Data do cálculo",
-            name: "dt_calculo",
-            render: renderDataCalculo,
-          },
-        ]}
-        rows={data?.historico ?? []}
-        loading={loading}
-      />
+      <VFlow>
+        <DataTable<Imc>
+          columns={[
+            {
+              header: "Altura (cm)",
+              name: ALTURA,
+              render: renderAltura,
+            },
+            {
+              header: "Peso (kg)",
+              name: PESO,
+              render: renderPeso,
+            },
+            {
+              header: "IMC",
+              name: "imc",
+              render: renderImc,
+            },
+            {
+              header: "Data do cálculo",
+              name: "dt_calculo",
+              render: renderDataCalculo,
+            },
+          ]}
+          rows={data?.historico ?? []}
+          loading={loading}
+        />
+        <Button onClick={() => executeImcQuery()} kind="primary">
+          Gerar histórico
+        </Button>
+      </VFlow>
     </>
   );
 }
