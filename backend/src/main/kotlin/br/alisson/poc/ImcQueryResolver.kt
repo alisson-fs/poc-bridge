3b562b9ef6987c1b2a4f02849d97c4492fb78469
AlissonFS <alisson.fs@bridge.ufsc.br>
2023-02-24 11:41:32 -0300

    Limitação de registros modificavel correto

--- backend/src/main/kotlin/br/alisson/poc/ImcQueryResolver.kt
+++ backend/src/main/kotlin/br/alisson/poc/ImcQueryResolver.kt
@@ -3,15 +3,17 @@ package br.alisson.poc
 import br.alisson.poc.model.Imc
 import br.alisson.poc.repository.ImcRepository
 import graphql.kickstart.tools.GraphQLQueryResolver
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Component
 
 @Component
 class ImcQueryResolver(private val repository: ImcRepository) : GraphQLQueryResolver {
 
-    fun historico(tamanho: Int): List<Imc> {
-        var size = tamanho
-        val all = repository.findAll()
-        if (size > all.count()) { size = all.count() }
-        return all.toList().subList(0, size)
+    fun historico(tamanho: Int): Page<Imc> {
+        val pageable = PageRequest.of(0, tamanho)
+        return repository.findAll(pageable)
     }
 }
--- backend/src/main/kotlin/br/alisson/poc/repository/ImcRepository.kt
+++ backend/src/main/kotlin/br/alisson/poc/repository/ImcRepository.kt
@@ -2,12 +2,12 @@ package br.alisson.poc.repository
 
 import br.alisson.poc.model.Imc
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import java.awt.print.Pageable
 
 @Repository
 interface ImcRepository: CrudRepository<Imc, Long> {
 
-    fun findByAltura(altura: Double, pageble: Pageable): Page<Imc>
+    fun findAll(pageble: Pageable): Page<Imc>
 }
