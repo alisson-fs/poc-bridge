88e90fd623d3909f0fb19419fa507b23aca44e01
AlissonFS <alisson.fs@bridge.ufsc.br>
2023-02-24 10:04:32 -0300

    WIP repository

--- backend/src/main/kotlin/br/alisson/poc/repository/ImcRepository.kt
+++ backend/src/main/kotlin/br/alisson/poc/repository/ImcRepository.kt
@@ -1,9 +1,13 @@
 package br.alisson.poc.repository
 
 import br.alisson.poc.model.Imc
+import org.springframework.data.domain.Page
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.awt.print.Pageable
 
 @Repository
 interface ImcRepository: CrudRepository<Imc, Long> {
+
+    fun findByAltura(altura: Double, pageble: Pageable): Page<Imc>
 }
